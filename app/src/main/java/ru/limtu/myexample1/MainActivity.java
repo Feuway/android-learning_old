b2060273aaa3679cfa99918d01a3b4faad09ff66
D.Smirnov <feuway@gmail.com>
2018-02-17 18:26:16 +0300

    complete practice-7

--- app/src/main/java/ru/limtu/myexample1/MainActivity.java
+++ app/src/main/java/ru/limtu/myexample1/MainActivity.java
@@ -2,12 +2,24 @@ package ru.limtu.myexample1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    String[] food = { "Пицца", "Фри", "Гамбургер", "Чизбургер", "Твистер" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView foodList = (ListView) findViewById(R.id.foodList);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, food);
+
+        foodList.setAdapter(adapter);
     }
 }

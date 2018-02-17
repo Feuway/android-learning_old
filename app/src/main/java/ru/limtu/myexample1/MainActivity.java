5c55d2152f293e8d9696f9753481af2fbc17da52
D.Smirnov <feuway@gmail.com>
2018-02-17 19:40:33 +0300

    complete practice-8

--- app/src/main/java/ru/limtu/myexample1/MainActivity.java
+++ app/src/main/java/ru/limtu/myexample1/MainActivity.java
@@ -1,7 +1,13 @@
 package ru.limtu.myexample1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(MainActivity.this, CategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setOnItemClickListener(itemClickListener);
     }
 }

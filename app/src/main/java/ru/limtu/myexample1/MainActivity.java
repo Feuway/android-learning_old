82508be8ff4dfb8922df833f5362e650a69fd403
D.Smirnov <feuway@gmail.com>
2018-02-17 12:05:34 +0300

    complete practice-3

--- app/src/main/java/ru/limtu/myexample1/MainActivity.java
+++ app/src/main/java/ru/limtu/myexample1/MainActivity.java
@@ -1,13 +1,34 @@
 package ru.limtu.myexample1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "hello, dude";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Метод обработки нажатия на кнопку
+     *
+     * @param view
+     */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessage.class);
+
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+
+        String message  = editText.getText().toString();
+
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+        startActivity(intent);
     }
 }
